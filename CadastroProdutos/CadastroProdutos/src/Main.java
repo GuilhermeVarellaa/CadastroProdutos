4cda7147f8e49ef5465cbf013979877dc9ef138a
Guilherme Varella <guilhermeraul1@hotmail.com>
2025-05-08 22:23:16 -0300

    feat: adiciona lista polimórfica com produtos físicos e digitais

--- CadastroProdutos/CadastroProdutos/src/Main.java
+++ CadastroProdutos/CadastroProdutos/src/Main.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Monitor", 150, 2);
 
-        Produto p2 = new ProdutoDigital("GTA VI", 600, 1, 100000);
+        List<Produto> produtos = new ArrayList<>();
 
-        Produto p3 = new ProdutoFisico("Mesa gamer", 600, 1, 50000);
+        produtos.add(new Produto("Celular", 100, 1));
+        produtos.add(new ProdutoFisico("Bicicleta", 790, 2, 5000));
+        produtos.add(new ProdutoDigital("GTA VI", 700, 1, 1000000));
 
-        p1.exibirDetalhes();
+        for (Produto p : produtos){
+            p.exibirDetalhes();
+        }
 
-        p2.exibirDetalhes();
 
-        p3.exibirDetalhes();
+//        Produto p1 = new Produto("Monitor", 150, 2);
+//
+//        Produto p2 = new ProdutoDigital("GTA VI", 600, 1, 100000);
+//
+//        Produto p3 = new ProdutoFisico("Mesa gamer", 600, 1, 50000);
+//
+//        p1.exibirDetalhes();
+//
+//        p2.exibirDetalhes();
+//
+//        p3.exibirDetalhes();
 
 
 
